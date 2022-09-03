6eb6016287a7bbcfb7b22df9762ab953bbe2a043
rashadmarkz <rashad.markcreative.com>
2022-09-03 17:29:42 +0530

    Expression & Variable - Do Your Self

---
+++ 01_Python Basics/04_DO_Your_Self(02).py
@@ -0,0 +1,21 @@
+# ____________________  Expressions ____________________
+"""
+     - Expression
+     - //
+     - Round()
+     - type()
+     - Assignment operator
+
+"""
+     
+
+a = 10
+b = 2
+c = 3.5
+d = 7.9
+
+print( a*b/c+d )
+
+sum = a*b//c+d
+print( round(sum) )
+print(type(sum))
