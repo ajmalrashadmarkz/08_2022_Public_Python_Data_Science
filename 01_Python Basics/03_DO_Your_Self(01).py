db2649b68a38997519ea376aab016cfadf6ae7b5
rashadmarkz <rashad.markcreative.com>
2022-09-03 17:01:18 +0530

    Data Types - Do Your Self

---
+++ 01_Python Basics/03_DO_Your_Self(01).py
@@ -0,0 +1,33 @@
+# ____________________ 01 - Types ____________________
+
+    # - type() Function
+
+a = 10
+b = 10.056
+c = "Rashad"
+d = "17"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+    # Typecasting
+
+print(int(b))
+print(float(a))
+print(int(d))
+
+
+    #Boolean
+
+age = True
+text = False
+
+print(type(age))
+print(type(text))
+print(type(True))
+print(type(False))
+
+print(age * text)
